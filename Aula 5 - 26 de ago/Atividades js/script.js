17d22c54857e476258261fbcc0e4bbf4e3002d89
FIC <fic@info.local>
2026-08-26 17:54:38 -0300

    atividades JS

---
+++ Aula 5 - 26 de ago/Atividades js/script.js
@@ -0,0 +1,103 @@
+// == compara valor
+// === compara valor e tipo
+// = atribuicao
+
+//let numero1 = Number (prompt ("Primeiro numero"))
+//let numero2 = Number (prompt ("Segundo numero"))
+
+//let resultado = numero1 + numero2;
+
+//let media = (numero1 + numero2) /2 //divisao por
+
+//console.log(media)
+
+//calcular imc que é peso por altura ao quadrado
+
+// let peso = Number(prompt ("Peso (Kg):"))
+// let altura = Number(prompt ("Altura (m):"))
+
+// let imc = peso / (altura ** 2)
+
+//console.log (imc.toFixed(2)) //tofixed casas apos a virgula
+//alert(imc.toFixed(2)) //popup
+
+
+// 18 - baixo
+// 18 e 24 - normal
+// 25 e 29 - sobrepeso
+
+//condicao
+
+// if ( imc <= 18){
+//     console.log(imc.toFixed + "baixo")
+//     alert(imc.toFixed(2) + abaixo)
+// } else if (imc > 18 && imc <= 24){
+//     console.log(imc.toFixed + "normal")
+//     alert(imc.toFixed(2) + normal)
+// } else {
+//     console.log(imc.toFixed + "sobrepeso")
+//     alert(imc.toFixed(2) + sobrepeso)
+// }
+
+//console.log(!true) //falso
+
+//&& - condicao todas as codnicoes devem ser TRUE
+//|| - se alguma condicao for certa, vai entrar no if
+//! - significa NAO. Ele inverte o valor de=o booleano.
+
+//console.log(!true) // false
+
+// let idade = 18
+
+// //se NAO idade maior ou igual a 18
+// if (!(idade >= 18)){
+//     console.log("Nao é maior de idade")
+// }
+
+//Atividade de 1 de js
+
+let idade = 20
+
+if (idade < 18){
+    console.log("É menor de idade")
+} else {
+    console.log("É maior de idade")
+}
+
+//Atividade 2
+let number = 5
+
+if (number < 0){
+    console.log("É numero negativo")
+} else {
+    console.log("É numero positivo")
+}
+
+//Atividade 3
+
+let nota = 85
+
+if (nota > 100){
+    console.log("Nota maxima 100, algo está errado")
+} else if (nota >= 60){
+    console.log("Aprovado")
+} else {
+    console.log("Reprovado")
+}
+
+//Atividade 4
+
+let numero = -5
+
+if (numero = 0){
+    console.log("Zero")
+} else if (numero >= 0){
+    console.log("Numero positivo")
+} else {
+    console.log("Numero negativo")
+}
+
+//Atividade 5
+
+ idade1 = prompt("Qual é a sua idade?");
+
