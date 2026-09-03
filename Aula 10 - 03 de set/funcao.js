8a616d51aa274815b0e81ebb275b722c9e38ebe9
FIC <fic@info.local>
2026-09-03 17:24:13 -0300

    Funcao js

---
+++ Aula 10 - 03 de set/funcao.js
@@ -0,0 +1,23 @@
+function Saudacao(){
+    console.log("Ola, seja bem vindo!!")
+}
+
+Solucao()
+Solucao()
+Solucao()
+Solucao()
+Solucao()
+Solucao()
+Solucao()
+Solucao()
+
+function SolicitarNome(){
+    
+
+let nomeInserido = prompt("Inserir nome")
+alert("O nome inserido eh " + nomeInserido)
+}
+
+SolicitarNome()
+SolicitarNome()
+SolicitarNome()
